f487793a910ca06994e233f7e0b1d897aeab146a
Aslkois <aslkois0424@gmail.com>
2026-09-05 15:47:49 -0400

    Fix data path for GitHub Pages

--- src/repository.ts
+++ src/repository.ts
@@ -17,7 +17,7 @@ function find_or_throw<T extends Thing>(things: T[], thing_id: string): T {
 
 const Repository = {
 	Init: async() => {
-		const response = await fetch('/data.json');
+		const response = await fetch('data.json');
 		database = Object.seal((await response.json()) as Database);
 		//add type to all things
 		database.resources.forEach(r => r.type = ThingType.Resource);
